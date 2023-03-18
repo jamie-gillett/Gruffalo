fc465ccf0f1cf74d0c8db8cc5edf44731094be79
Jamie <jamie.gillett@gmail.com>
2023-03-18 23:01:36 +0000

    prior to removing branch for descriptions

--- gruffalo.py
+++ gruffalo.py
@@ -7,20 +7,20 @@ class Animal:
     meal: str
     home: str
     compliment: str
-    # descriptions: list[str]
+    descriptions: list[str]
     # meeting: str
     # fave_food: str
     # exclamation: str
     # motion: str
 
 animals = [
-    Animal("Fox","lunch","underground","terribly kind"),
-    Animal("Owl","tea","treetop","frightfully nice"),
-    Animal("Snake","a feast","logpile","wonderfully good")
+    Animal("Fox", "lunch", "underground", "terribly kind", ["terrible tusks", "terrible claws", "terrible teeth in his terrible jaws"]),
+    Animal("Owl", "tea", "treetop", "frightfully nice", ["knobbly knees", "turned-out toes", "a poisonous wart at the end of his nose"]),
+    Animal("Snake", "a feast", "logpile", "wonderfully good", ["eyes are orange", "tongue is black", "purple prickles all over his back"])
 ]
 
 # ------ STORY TIME ------ #
-print()
+print() # First encounters
 for animal in animals:
     print(f'{"A mouse took a stroll" if animal.name=="Fox" else "On went the mouse"} through the deep dark wood.')
     print(f'A{"n" if animal.name=="Owl" else ""} {animal.name.lower()} saw the mouse and the mouse looked good.')
@@ -30,4 +30,12 @@ for animal in animals:
     print(f"I'm {'having' if animal.name=='Snake' else 'going to have'} {animal.meal} with a gruffalo.\"")
     print("\"A gruffalo? What's a gruffalo?\"")
     print("\"A gruffalo! Why, didn't you know?")
+    # if not animal.name == "Snake":
+    #     print(f'"He has {animal.descriptions[0]}, and {animal.descriptions[1]},')
+    #     print(f'And {animal.descriptions[2]}."')
+    # else:
+    #     print(f'"His {animal.descriptions[0]}, his {animal.descriptions[1]};')
+    #     print(f'He has {animal.descriptions[2]}."')
+    print(f'"{"He has" if not animal.name=="Snake" else "His"} {animal.descriptions[0]}, {"and" if not animal.name=="Snake" else "his"} {animal.descriptions[1]}{"," if not animal.name=="Snake" else ";"}')
+    print(f'{"And" if not animal.name=="Snake" else "He has"} {animal.descriptions[2]}."')
     print()
