38c095b1d3bad327e8b1c2a630f72a58cd5c3ab4
Jamie <jamie.gillett@gmail.com>
2023-03-18 23:41:00 +0000

    ..what happened to owl?!?!

--- gruffalo.py
+++ gruffalo.py
@@ -8,15 +8,15 @@ class Animal:
     home: str
     compliment: str
     descriptions: list[str]
-    # meeting: str
-    # fave_food: str
-    # exclamation: str
-    # motion: str
+    meeting: str
+    fave_food: str
+    exclamation: str
+    motion: str
 
 animals = [
-    Animal("Fox", "lunch", "underground", "terribly kind", ["terrible tusks", "terrible claws", "terrible teeth in his terrible jaws"]),
-    Animal("Owl", "tea", "treetop", "frightfully nice", ["knobbly knees", "turned-out toes", "a poisonous wart at the end of his nose"]),
-    Animal("Snake", "a feast", "logpile", "wonderfully good", ["eyes are orange", "tongue is black", "purple prickles all over his back"])
+    Animal("Fox", "lunch", "underground", "terribly kind", ["terrible tusks", "terrible claws", "terrible teeth in his terrible jaws"], "rocks", "Roasted fox", "I'm off", "sped"),
+    Animal("Owl", "tea", "treetop", "frightfully nice", ["knobbly knees", "turned-out toes", "a poisonous wart at the end of his nose"], "stream", "Owl ice cream", "Toowhit toowhoo", "flew"),
+    Animal("Snake", "a feast", "logpile", "wonderfully good", ["eyes are orange", "tongue is black", "purple prickles all over his back"], "lake", "Scrambled snake", "It's time I hid", "slid")
 ]
 
 # ------ STORY TIME ------ #
@@ -30,6 +30,23 @@ for animal in animals:
     print(f"I'm {'having' if animal.name=='Snake' else 'going to have'} {animal.meal} with a gruffalo.\"")
     print("\"A gruffalo? What's a gruffalo?\"")
     print("\"A gruffalo! Why, didn't you know?")
+    print() # page break
     print(f'"{"His" if animal.name=="Snake" else "He has"} {animal.descriptions[0]}, {"his" if animal.name=="Snake" else "and"} {animal.descriptions[1]}{";" if animal.name=="Snake" else ","}')
     print(f'{"He has" if animal.name=="Snake" else "And"} {animal.descriptions[2]}."')
-    print()
+    print('"Where are you meeting him?"')
+    print(f'"Here, by {"these" if animal.name=="Fox" else "this"} {animal.meeting},')
+    print(f'And his favourite food is {animal.fave_food.lower()}."')
+    print() # page break
+    if animal.name=="Fox":
+        print(f"\"{animal.fave_food}! {animal.exclamation}!\" Fox said.")
+        print("\"", end='')
+    else:
+        print(f"\"{animal.fave_food}! {animal.exclamation}!")
+    print(f'Goodbye, little mouse," and away {"he" if animal.name=="Fox" else animal.name} {animal.motion}.')
+    print(f'"Silly old {animal.name}! Doesn\'t he know,')
+    print(f'There\'s no such thing as a gruffal',end='')
+    if animal.name=="Snake":
+        print('...')
+    else:
+        print('o?"')
+    print() # page break
