3ee03f942c770aedab1df3e4712150fa18321fe9
Jamie <jamie.gillett@gmail.com>
2023-03-18 22:43:07 +0000

    migrated to dataclasses

--- gruffalo.py
+++ gruffalo.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+# ------ SET UP ------ #
 @dataclass
 class Animal:
     name: str
@@ -18,19 +19,15 @@ animals = [
     Animal("Snake","a feast","logpile","wonderfully good")
 ]
 
-animals = ["Fox", "Owl", "Snake"]
-meals = {"Fox": "lunch", "Owl": "tea", "Snake": "a feast"}
-homes = {"Fox": "underground", "Owl": "treetop", "Snake": "logpile"}
-compliment = {"Fox": "terribly kind", "Owl": "frightfully nice", "Snake": "wonderfully good"}
-
+# ------ STORY TIME ------ #
 print()
-for i, animal in enumerate(animals):
-    print(f'{"A mouse took a stroll" if animal=="Fox" else "On went the mouse"} through the deep dark wood.')
-    print(f'A{"n" if animal=="Owl" else ""} {animal.lower()} saw the mouse and the mouse looked good.')
+for animal in animals:
+    print(f'{"A mouse took a stroll" if animal.name=="Fox" else "On went the mouse"} through the deep dark wood.')
+    print(f'A{"n" if animal.name=="Owl" else ""} {animal.name.lower()} saw the mouse and the mouse looked good.')
     print('"Where are you going to, little brown mouse?')
-    print(f'Come {"for" if animal=="Snake" else "and have"} {meals[animal]} in my {homes[animal]} house."')
-    print(f"\"It's {compliment[animal]} of you, {animal}, but no --")
-    print(f"I'm {'having' if animal=='Snake' else 'going to have'} {meals[animal]} with a gruffalo.\"")
+    print(f'Come {"for" if animal.name=="Snake" else "and have"} {animal.meal} in my {animal.home} house."')
+    print(f"\"It's {animal.compliment} of you, {animal.name}, but no --")
+    print(f"I'm {'having' if animal.name=='Snake' else 'going to have'} {animal.meal} with a gruffalo.\"")
     print("\"A gruffalo? What's a gruffalo?\"")
     print("\"A gruffalo! Why, didn't you know?")
     print()
